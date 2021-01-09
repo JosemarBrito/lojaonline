311ce8638ec2af193120076e5ae2f7972962820b
josemar <estudos.josemarbrito@gmail.com>
2021-01-08 22:42:27 -0300

    Configurado lib python decouple close #11

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0t@pz@o$j(-j$b68)(cyi%d$%3nh5s&r-w0ecy03n0n%2v^tfa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
