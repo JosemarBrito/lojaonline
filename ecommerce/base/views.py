00a10737cb52411fdedcc9175ac28ca063c5f8db
josemar <estudos.josemarbrito@gmail.com>
2021-01-14 22:22:55 -0300

    Configurado Banco de dados postgres close #19

--- ecommerce/base/views.py
+++ ecommerce/base/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Filhas do Rei!</h1>')
+    return HttpResponse('<h1>Todos somos capazes de fazer melhor a cada dia</h1>')
