e7e5698b6d3a6a24263aad8a8934effa784dbb51
josemar <estudos.josemarbrito@gmail.com>
2021-01-20 21:21:04 -0300

    Configuração do collectfast
    close #29

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'ecommerce.base',
 ]
@@ -131,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
-# COLLECTFAST_ENABLED = False
+COLLECTFAST_ENABLED = False
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
@@ -147,7 +148,7 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
     AWS_DEFAULT_ACL = 'private'
-    #  COLLECTFAST_ENABLED = True
+    COLLECTFAST_ENABLED = True
 
     # Static Assert
     # ____________________________________________________________________________
